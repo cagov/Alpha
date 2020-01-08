871b3cf1c598e7b95c599fa280a4527c13672504
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-01-08 13:23:26 -0800

    add font display swap

---
+++ public/js/styles.0d8654fa7ce90c6ecac7.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{8:function(n,w,o){}}]);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const minificationOptions = {
 }
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
     javascript: [
       './src/js/index.js'
