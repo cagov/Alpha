033f717ccde81a838e2580d63151c847bdd6a3d1
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-01-10 09:10:36 -0800

    now working with edge stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 import './libs/awesomplete.js'
+
+// import 'document-register-element';
+// this is only needed for pre chromium edge because everybody else gets nomodule es5 transpiled build, loading it in the favicons.html for now
+
 import './components/accordion/index.js'
 import './components/step-list/index.js'
