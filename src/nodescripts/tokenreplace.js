f2c0438c9150fb73bd547bf689a560d309afdd80
carterm <carter.medlin@gmail.com>
2020-01-24 13:10:51 -0800

    working

--- src/nodescripts/tokenreplace.js
+++ src/nodescripts/tokenreplace.js
@@ -2,18 +2,19 @@ const fs = require('fs')
 const fse = require('fs-extra') //https://www.npmjs.com/package/fs-extra
 const csv = require('csv-parser') //https://www.npmjs.com/package/csv-parser
 const replace = require('replace-in-file'); //https://www.npmjs.com/package/replace-in-file
+const sourcefolder = 'public/'
 
 const globalfilepath = 'src/lang-global.csv'
 var targetlangs = ['es','en']
 
 //start by copying the existing language output to a source folder
 
-const source = 'public/langsrc'
-fs.renameSync('public/en',source)
+const source = sourcefolder + 'langsrc'
+fs.renameSync(sourcefolder+'en',source)
 
 for(const targetlang of targetlangs) {
 
-  const destination = 'public/'+targetlang
+  const destination = sourcefolder+targetlang
   
   const files = destination+'/**/*.html'
   
@@ -21,13 +22,13 @@ for(const targetlang of targetlangs) {
   const from = [
     /lang="en"/g, 
     /\/en\//g,
-    /Alp[A-Za-z-]+/g
+    ///Alp[A-Za-z-]+/g
   ]
   
   const to = [
     'lang="'+targetlang+'"', 
     /es/,
-    (match, ...args) => 'Match='+ match +'file='+args.pop()
+    //(match, ...args) => 'Match='+ match +'file='+args.pop()
   ]
   
   
@@ -35,8 +36,22 @@ for(const targetlang of targetlangs) {
   fs.createReadStream(globalfilepath)
     .pipe(csv())
     .on('data', (data) => {
-      from.push(data.token)
-      to.push(data[targetlang])
+      var token = data.token.replace(/\[/,'\\\[').replace(/\]/,'\\\]')
+
+      from.push(new RegExp(token,'g'))
+
+      if(data.path)
+        to.push(function (match, ...args) {
+            const file=args.pop().replace(/index.html$/,'').replace(destination.replace(/\//,'\/'),'')
+
+            //return file
+            return data.path==file ? data[targetlang] : match
+          
+        })
+      else
+        to.push(data[targetlang])
+
+
     })
     .on('end', () => {
   
