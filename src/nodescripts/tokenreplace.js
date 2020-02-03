bdd7de1c5ee5f14c21eb6d47e46db819f389bd85
carterm <carter.medlin@gmail.com>
2020-02-03 10:47:00 -0800

    interesting with token finding

--- src/nodescripts/tokenreplace.js
+++ src/nodescripts/tokenreplace.js
@@ -17,7 +17,6 @@ const targetlangs = [
 const source = sourcefolder + 'langsrc'
 fs.renameSync(sourcefolder+'en',source)
 
-
 for(const targetlangobject of targetlangs) {
   const targetlang = targetlangobject.code
 
@@ -30,59 +29,72 @@ for(const targetlangobject of targetlangs) {
 
   const destination = sourcefolder+targetlang
 
-  const fileFromArgs = args => args.pop()
-    .replace(/\/index.html$/,'') //Remove index.html
-    .replace(new RegExp('^'+destination.replace(/\//,'\/')),'') //Remove "public/en"
-  
-  const files = destination+'/**/*.html'
-  
-  const from = [
-    /lang="en"/g,
-    /\/en\//g,
-    /\[code-language-select\]/g,
-    /\[FullPath\]/g
-  ]
-  
-  const to = [
-    'lang="'+targetlang+'"', 
-     targetlang=='en'?'/':'/'+targetlang+'/',
-     langselectorbutton,
-     (match, ...args) => fileFromArgs(args)
-  ]
-  
-  const results = [];
-  fs.createReadStream(globalfilepath, {encoding: 'utf16le'})
-    .pipe(csv({ separator: '\t', strict: true, skipComments: true, newline: '\r\n' }))
-    .on('data', data => {
-
-      if(data.token&&data[targetlang]) {
-        from.push(new RegExp(data.token.replace(/\[/,'\\\[').replace(/\]/,'\\\]'),'g')) //add token with literal square brackets
-
-        if(data.path)
-          to.push( (match, ...args) => {
-              let file=fileFromArgs(args)
-
-              if(!file)
-                file ='/'
-
-              //return text, or the original match if the file isn't right
-              return data.path==file ? data[targetlang] : match
-            
-          })
-        else
-          to.push(data[targetlang])
-    }})
-    .on('end', _ => 
-    
   // copy source folder to destination
   fse.copy(source, destination, {overwrite: false, errorOnExist: true}, err => {
-      if (err) return console.error(err)
-  
-      // Replace HTML Language
-      replace({files,from,to}, (error, results) => {
-        if (error) return console.error(error);
+    if (err) return console.error(err)
 
-        //English default goes to root
+    const fileFromArgs = args => args.pop()
+      .replace(/\/index.html$/,'') //Remove index.html
+      .replace(new RegExp('^'+destination.replace(/\//,'\/')),'') //Remove "public/en"
+    
+    const files = destination+'/**/*.html'
+    
+    const defaultfrom = [
+      /lang="en"/g,
+      /\/en\//g,
+      /\[code-language-select\]/g,
+      /\[FullPath\]/g
+    ]
+    
+    const defaultto = [
+      'lang="'+targetlang+'"', 
+      targetlang=='en'?'/':'/'+targetlang+'/',
+      langselectorbutton,
+      (match, ...args) => fileFromArgs(args)
+    ]
+  
+    replace({files,from:defaultfrom,to:defaultto}, (error, results) => {
+      if (error) return console.error(error);
+
+      fs.createReadStream(globalfilepath, {encoding: 'utf16le'})
+        .pipe(csv({ separator: '\t', strict: true, skipComments: true, newline: '\r\n' }))
+        .on('data', data => {
+
+          if(data.token&&data[targetlang]) {
+            const from = [new RegExp(data.token.replace(/\[/,'\\\[').replace(/\]/,'\\\]'),'g')] //add token with literal square brackets
+            let to = []
+            if(data.path)
+              to = (match, ...args) => {
+                  let file=fileFromArgs(args)
+
+                  if(!file)
+                    file ='/'
+
+                  //return text, or the original match if the file isn't right
+                  return data.path==file ? data[targetlang] : match
+                
+              }
+            else
+              to = data[targetlang]
+            
+              
+            replace({files,from,to}, (error, results) => {
+                if (error) return console.error(error);
+
+                let found = false
+                results.forEach(element => {
+                  if (element.hasChanged)
+                    found = true
+                });
+
+                if(!found)
+                  return console.error('replacement not found - '+data.token);
+
+                //console.log(results);
+            })
+        }})
+        .on('end', _ => {
+          //English default goes to root
         if(targetlang=='en') 
           fse.copy(destination, sourcefolder, {overwrite: true, errorOnExist: false}, err => {
             if (err) return console.error(err)
@@ -91,6 +103,6 @@ for(const targetlangobject of targetlangs) {
         //console.log('Lang Replacement results:', results);
         console.log(targetlang + ': Language Replacement Complete:');
       })
-    })
-  );
+    })  
+  })
 }
