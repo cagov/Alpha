8797518002a04f5b6e9872ba582da66f0e6f0963
carterm <carter.medlin@gmail.com>
2020-02-24 13:22:29 -0800

    Holidays uses service for dates (and translations)

--- src/js/components/holidays/index.js
+++ src/js/components/holidays/index.js
@@ -23,18 +23,35 @@ class CWDSHolidays extends HTMLElement {
 
       const fulldate = nextrow.dateobject.toLocaleDateString(locales, { weekday: 'long', month: 'long', day: 'numeric' })
 
-
-      document.getElementById("current-holiday-date").innerText=fulldate //day_of_week+', '+month_name+' '+day_of_month
+      document.getElementById("current-holiday-date").innerText=fulldate
       document.getElementById("current-holiday-name").innerText=nextrow.name
+
+
+      const template = document.getElementById("js-template")
+      //const results = document.getElementById("js-tbody-template-2020-results")
+
+      //<td><span class="data-holiday-date">Date</span><span class="d-none data-holiday-credit-link">(<a href="#credits">holiday credit</a>)</span></td>
+      //<td class="data-holiday-day-of-week">day of week</td>
+      //<td class="data-holiday-name">name</td>
+
+      const addrows = (targetcontainer,year) => {
+        for (const row of data.filter(x=>x.dateobject.getFullYear()===year)) {
+          const node = template.content.cloneNode(true)
+          node.querySelector(".data-holiday-date").innerText=row.dateobject.toLocaleDateString(locales, { month: 'long', day: 'numeric' })
+          node.querySelector(".data-holiday-day-of-week").innerText=row.dateobject.toLocaleDateString(locales, { weekday: 'long' })
+          node.querySelector(".data-holiday-name").innerText=row.name
+
+          if(row.dateobject.getDay()===0 || row.dateobject.getDay()===6)
+            node.querySelector(".data-holiday-credit-link").classList.remove('d-none');
+          
+  
+          targetcontainer.appendChild(node)
+        }
+      }
+
+      addrows(document.getElementById("js-tbody-template-2020-results"),2020)
+      addrows(document.getElementById("js-tbody-template-2021-results"),2021)
     })
-
-
-
-
-
-
-
-
   }
 }
 window.customElements.define('cwds-holidays', CWDSHolidays)
