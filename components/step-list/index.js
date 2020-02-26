2f8a8778b3638e6b78900c02e9cfbc53847ed762
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-02-26 10:28:50 -0800

    bump step list to remove strict mode errors

--- components/step-list/index.js
+++ components/step-list/index.js
@@ -29,12 +29,9 @@ window.customElements.define('cwds-step-list', CWDSStepList);
 /* expand, collapse thanks to: https://css-tricks.com/using-css-transitions-auto-dimensions/ */
 function collapseSection(element) {
   var sectionHeight = element.scrollHeight;
-  var elementTransition = element.style.transition;
-  element.style.transition = '';
 
   requestAnimationFrame(function() {
     element.style.height = sectionHeight + 'px';
-    element.style.transition = elementTransition;
     
     requestAnimationFrame(function() {
       element.style.height = 0 + 'px';
@@ -47,11 +44,5 @@ function collapseSection(element) {
 function expandSection(element) {
   var sectionHeight = element.scrollHeight;
   element.style.height = sectionHeight + 'px';
-
-  element.addEventListener('transitionend', function(e) {
-    element.removeEventListener('transitionend', arguments.callee);
-    element.style.height = null;
-  });
-  
   element.setAttribute('data-collapsed', 'false');
 }
