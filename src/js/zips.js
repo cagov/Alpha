f837a5ed69ba9aa86bd6a027c35d41cb773724c0
Aaron Hans <aaronhans@gmail.com>
2020-03-02 15:32:15 -0800

    split showall function (#558)

--- src/js/zips.js
+++ src/js/zips.js
@@ -3,6 +3,7 @@ import '../../node_modules/@cagov/accordion'
 import '../../node_modules/@cagov/step-list'
 import './components/holidays/index.js'
 import './libs/tooltip.js'
+import './util/show-all.js'
 // above duplicated from index.js
 
 // below all unique zip array dependent files
