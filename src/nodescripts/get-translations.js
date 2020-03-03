efcad6131b02c72ef72daad20a4723c111ecdfaf
carterm <carter.medlin@gmail.com>
2020-03-03 12:44:29 -0800

    fixed lang update speed

--- src/nodescripts/get-translations.js
+++ src/nodescripts/get-translations.js
@@ -29,6 +29,6 @@ const download = (url, dest, cb) => {
     });
 };
 
-download('https://docs.google.com/spreadsheets/d/e/2PACX-1vT8aUVX-O2vt3Z0y3k0pipxtILfx7E5VFk4yKGOm_nniHF8DNAeaZcM_yMma8rpOP0xjv9N8fi9lPGo/pub?output=csv','./src/lang-global.csv',function(msg) {
+download('https://docs.google.com/spreadsheets/d/e/2PACX-1vT8aUVX-O2vt3Z0y3k0pipxtILfx7E5VFk4yKGOm_nniHF8DNAeaZcM_yMma8rpOP0xjv9N8fi9lPGo/pub?output=csv&extra='+(new Date().getTime().toString()),'./src/lang-global.csv',function(msg) {
   console.log('done')
 })
