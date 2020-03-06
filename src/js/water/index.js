c85c9f4f08dc2d1fa4b3f1ca7e7741443e943446
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-03-06 10:51:31 -0800

    lint fixes

--- src/js/water/index.js
+++ src/js/water/index.js
@@ -1,5 +1,5 @@
 import gotSystem from './got-system.js';
-import getParameterByName from './getparams.js';
+import getParameterByName from '../util/get-params.js';
 import Awesomplete from 'awesomplete-es6';
 
 if (document.querySelector('body.js-water')) {
@@ -23,54 +23,17 @@ if (document.querySelector('body.js-water')) {
     }
   });
 
-  function queryLoc (q) {
-    window.lookup = q;
-    const url = `https://api.alpha.ca.gov/alphageotypeahead?onlyca=true&q=${q}`;
-    window.fetch(url)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const list = [];
-        data.match.forEach((item) => {
-          list.push(item.address);
-        });
-        window.waterPlete.list = list;
-      })
-      .catch(error => {
-        resetForm();
-      });
-  }
-
   document.querySelector(fieldSelector).addEventListener('keydown', event => {
     if (event.isComposing || event.keyCode === 229) {
       return;
     }
-    let skipKeys = [13, 9, 27, 38, 40]
+    const skipKeys = [13, 9, 27, 38, 40]; // do not reset suggestion list if using arrow keys, enter, tab
     if (event.target.value.length >= 2) {
-      if(skipKeys.indexOf(event.keyCode) === -1) {
+      if (skipKeys.indexOf(event.keyCode) === -1) {
         queryLoc(event.target.value);
       }
     }
   });
-
-  function resetForm () {
-    document.querySelector('.js-water-lookup').innerHTML = 'Check your water';
-    document.querySelector('.system-data').style.display = 'block';
-  }
-
-  function answerChosen (item) {
-    const waterButton = document.querySelector('.js-water-lookup');
-
-    const template = document.getElementById('loading');
-    const node = template.content.cloneNode(true);
-
-    waterButton.innerHTML = node.querySelector('div').innerHTML;
-    document.querySelector('.system-data').style.display = 'none';
-
-    // make call to endpoint to find system
-    retrieveSystemData(item);
-  }
 
   document
     .querySelector('.js-water-form')
@@ -78,21 +41,6 @@ if (document.querySelector('body.js-water')) {
       event.preventDefault();
       document.querySelector('.invalid-feedback').style.display = 'none';
     });
-
-  function retrieveSystemData (item) {
-    window.fetch(
-      `https://api.alpha.ca.gov/WaterSystem?stringLoc=${item}`
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(systemData => {
-        gotSystem(systemData);
-      })
-      .catch(error => {
-        resetForm();
-      });
-  }
 
   if (getParameterByName('systemId')) {
     const url = `https://api.alpha.ca.gov/WaterSystem?systemId=${getParameterByName(
@@ -106,6 +54,58 @@ if (document.querySelector('body.js-water')) {
         gotSystem(systemData);
         document.querySelector('.system-data').style.display = 'block';
       })
-      .catch(error => {});
+      .catch(() => {});
   }
+}
+
+function answerChosen (item) {
+  const waterButton = document.querySelector('.js-water-lookup');
+
+  const template = document.getElementById('loading');
+  const node = template.content.cloneNode(true);
+
+  waterButton.innerHTML = node.querySelector('div').innerHTML;
+  document.querySelector('.system-data').style.display = 'none';
+
+  // make call to endpoint to find system
+  retrieveSystemData(item);
+}
+
+function queryLoc (q) {
+  window.lookup = q;
+  const url = `https://api.alpha.ca.gov/alphageotypeahead?onlyca=true&q=${q}`;
+  window.fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const list = [];
+      data.match.forEach((item) => {
+        list.push(item.address);
+      });
+      window.waterPlete.list = list;
+    })
+    .catch(() => {
+      resetForm();
+    });
+}
+
+function resetForm () {
+  document.querySelector('.js-water-lookup').innerHTML = 'Check your water';
+  document.querySelector('.system-data').style.display = 'block';
+}
+
+function retrieveSystemData (item) {
+  window.fetch(
+    `https://api.alpha.ca.gov/WaterSystem?stringLoc=${item}`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(systemData => {
+      gotSystem(systemData);
+    })
+    .catch(() => {
+      resetForm();
+    });
 }
